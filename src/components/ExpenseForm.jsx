f9a1377b8a5363f69cbd0077232d97ab44459670
Juan Quino <quinojuan@gmail.com>
2023-03-11 15:27:43 -0300

    form

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,5 +1,32 @@
+import { MdSend } from "react-icons/md";
+
 const ExpenseForm = () => {
-  return <div>Hello from ExpenseForm</div>;
+  return (
+    <form action="">
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="e.g. rent"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">amount</label>
+          <input
+            type="text"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="e.g. 100"
+          />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
